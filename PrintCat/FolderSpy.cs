523f01ec1c52b4c0e41e1817a5ea9632d447993f
hxshandle <handle.huang0409@gmail.com>
2014-07-13 20:32:26 +0800

    good

---
+++ PrintCat/FolderSpy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PrintCat
+{
+  class FolderSpy
+  {
+    private List<FileInfo> imageList = new List<FileInfo>();
+
+    static String spyPath = "g:\\tmp";
+
+
+    public FolderSpy()
+    {
+      FileSystemWatcher fsw = new FileSystemWatcher();
+      fsw.Path = spyPath;
+      fsw.Filter = "*.png|*.jpeg|*.jpg";
+    }
+
+
+  }
+}
